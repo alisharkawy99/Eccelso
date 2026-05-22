6131793acb6159d0f940f18127ffba44b498675b
alisharkawy99 <alisharkawy1999@gmail.com>
2026-05-23 01:51:09 +0300

    feat:Added the env file for frontend and added the cors policy

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from routers.cars import router as cars_router
 
+origins = [
+    "http://localhost:3000",  # Next.js / React default
+    "http://localhost:5173",  # Vite default
+]
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows GET, POST, PUT, PATCH, DELETE
+    allow_headers=["*"],
+)
+
 
 app.include_router(cars_router)
 
